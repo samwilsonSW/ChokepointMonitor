9f6ac18516e474875b80666b3c4832d33119991b
Sam Wilson <sam.wilson@ttu.edu>
2026-01-11 17:07:27 -0600

    Add python main file

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from parseACLED.py import read_excel_file
+
+def main():
+  print(read_excel_file("/Users/duk/Desktop/Chokepoint Monitor/datasets/Africa_aggregated_data_up_to-2025-12-27.xlsx"))
+
+
+
+
+
+if __name__ == "__main__":
+    main()
