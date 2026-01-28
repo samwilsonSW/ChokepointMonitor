83d41f13de1acf9d7da9a404fd71115723a18647
Sam Wilson <samwilsonswbiz@gmail.com>
2026-01-28 22:47:31 +0000

    Add map from MapLibre GL and MapTiler

---
+++ js/map.js
@@ -0,0 +1,18 @@
+// paste api key from MAPTILER here
+const API_KEY = '4DXmdK3KMIwSxqxWSMo8';
+const MAP_STYLE = `https://api.maptiler.com/maps/dataviz/style.json?key=${API_KEY}`;
+
+// initialize maplibre here: https://maplibre.org/maplibre-gl-js/docs/
+const map = new maplibregl.Map({
+    container: 'map', // Matches the ID in index.html
+    style: MAP_STYLE, // The Basemap (Dataviz Theme)
+    center: [55.0, 18.0], // Longitude, Latitude (Centered on Arabian Sea/Oman)
+    zoom: 5, // Zoom level (Shows Hormuz to Bab el-Mandeb)
+    attributionControl: false // Cleaner look (we add attribution manually if needed later)
+});
+
+// 3. Lifecycle Hooks
+map.on('load', () => {
+    console.log("Map Loaded Successfully");
+    // This is where we will eventually load our Conflict & Tanker layers
+});
